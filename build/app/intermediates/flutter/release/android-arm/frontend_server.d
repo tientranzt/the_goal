21fdb51ed6b2e8931cd578f299d5a788ad5ec1ab
tientranzt <tientran.home@gmail.com>
2019-12-05 13:30:26 +0700

    add build folder

---
+++ build/app/intermediates/flutter/release/android-arm/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: C:\Flutter\flutter_windows_v1.9.1+hotfix.4-stable\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
---
+++ build/app/intermediates/flutter/release/android-arm/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: C:\Flutter\flutter_windows_v1.9.1+hotfix.4-stable\bin\cache\artifacts\engine\android-arm-release\windows-x64\gen_snapshot
